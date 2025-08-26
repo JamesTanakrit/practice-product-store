074ba93a7785c0b4984a391d069a557ba431367b
JamesTanakrit <88365807+JamesTanakrit@users.noreply.github.com>
2025-08-27 02:57:08 +0700

    build product api

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
 const port = 3000;
 
-app.get("/products", (req, res) => {
-  res.send("List of products");
-});
+app.use(express.json()); // Middleware to parse JSON request bodies
+
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => {
   connectDB();
