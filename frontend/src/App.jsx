42c4488becea7a646f8abc18cf3be2ffc72e6554
JamesTanakrit <88365807+JamesTanakrit@users.noreply.github.com>
2025-08-27 21:50:14 +0700

    create react-router page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
-import { Button } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
-      <Button>hello</Button>
-    </>
+    <Box minH={"100vh"}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
   );
 }
 
